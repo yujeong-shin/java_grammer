d11d1d69da8a0a1f19334ef48995d31e45670af3
yujeong-shin <vouobb@naver.com>
2023-12-15 15:51:12 +0900

    강사님 코드 루팡 - 객체를 찾는 점이 가장 큰 차이점!! - 활용도 높음

--- src/C12ClassLecture/AuthorPostService.java
+++ src/C12ClassLecture/AuthorPostService.java
@@ -5,115 +5,95 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AuthorPostService {
-    //    인스턴스 변수 생성
-//    생성자 : 모든 인스턴스 변수 초기화
-//    모든 클래스에 getter 생성
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         List<Author> authors = new ArrayList<>();
         List<Post> posts = new ArrayList<>();
-        boolean checkDuplicate = false;
-        while(true){
-            System.out.println("기능을 선택해주세요");
-            System.out.println("1 : 회원가입, 2 : 게시글 작성, 3 : 회원 목록조회, 4 : 회원 상세조회, 5 : 게시글 상세조회");
-            int menu = Integer.parseInt(sc.nextLine());
-            if(menu == 1) {
-                //회원가입
-                System.out.println("회원가입하실 이름을 입력해주세요");
-                String name = sc.nextLine();
 
-                System.out.println("회원가입하실 이메일을 입력해주세요");
-                String email = sc.nextLine();
-                if(authors.size()>=0) {
-                    Author myAuthor = new Author(name, email);
-                    authors.add(myAuthor);
-                    System.out.println("현재까지 가입자 수는 : " + authors.size());
-                    System.out.println(myAuthor.toString());
-                }
-                else {
-                    for(Author a : authors) {
-                        if (a.getEmail().equals(email)) {
-                            System.out.println("이미 존재하는 이메일입니다. 다시 회원가입을 진행해주세요");
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            int number = Integer.parseInt(sc.nextLine());
+            System.out.println("1번:회원가입, 2번:게시글작성, 3번:회원목록조회, 4번:회원상세조회, 5번:게시글상세조회 ");
+            switch (number){
+                case 1:
+                    System.out.println("회원가입하실 이름을 입력해주세요");
+                    String name = sc.nextLine();
+                    System.out.println("회원가입하실 이메일을 입력해주세요");
+                    String email = sc.nextLine();
+                    Author author1 = new Author(name, email);
+                    authors.add(author1);
+                    break;
+                case 2:
+                    System.out.println("게시글명을 입력해주세요");
+                    String title = sc.nextLine();
+                    System.out.println("게시글 내용을 입력해주세요");
+                    String contents = sc.nextLine();
+                    System.out.println("작성자 id를 입력해주세요");
+                    Long id = Long.parseLong(sc.nextLine());
+                    Post post1 = new Post(title, contents, id);
+                    posts.add(post1);
+                    break;
+//                    회원목록조회
+                case 3:
+                    for(Author a : authors){
+                        System.out.println(a.getEmail());
+                    }
+                    break;
+//                    회원상세조회 : 이름, email, 작성글수
+                case 4:
+                    System.out.println("author email를 입력해주세요");
+                    String author_email = sc.nextLine();
+                    Author temp_author = null;
+                    for(Author a : authors){
+                        if(a.getId().equals(author_email)){
+                            temp_author = a;
                             break;
                         }
                     }
-                }
-            }
-            else if(menu == 2) {
-                //게시글 작성
-                //이메일로 가입여부 확인
-                System.out.println("회원 이메일을 입력해주세요");
-                String input_email = sc.nextLine();
-                for(Author a : authors) {
-                    if (a.getEmail().equals(input_email)) {
-                        System.out.println("회원 가입한 회원입니다");
-
-                        System.out.println("게시글 제목을 입력해주세요");
-                        String title = sc.nextLine();
-                        System.out.println("게시글 내용을 입력해주세요");
-                        String contents = sc.nextLine();
-                        Post myPost = new Post(title, contents, a.getId());
-                        posts.add(myPost);
-                        System.out.println("게시글 작성이 완료됐습니다");
-                        System.out.println(myPost.toString());
-                    }
-                    else {
-                        System.out.println("먼저 회원 가입을 진행해주세요");
-                        break;
-                    }
-                }
-            }
-            else if(menu == 3) {
-                for(Author a : authors) {
-                    System.out.println(a.getEmail());
-                }
-                System.out.println();
-            }
-            else if(menu == 4) {
-                //회원명, 회원email, 회원작성글수
-                for(Author a : authors) {
-                    System.out.println("name : " + a.getName());
-                    System.out.println("email : " + a.getEmail());
-                    int post_count=0;
+                    int count = 0;
                     for(Post p : posts){
-                        if(p.getAuthor_id() == a.getId()) post_count++;
+                        if(p.getAuthor_id() == temp_author.getId()){
+                            count++;
+                        }
                     }
-                    System.out.println("count posts : " + post_count);
-                    System.out.println();
-                }
-            }
-            else if(menu == 5) {
-                for(Post p : posts){
-                    System.out.println("게시글 제목 : " + p.getTitle());
-                    //post의 author_id와 author의 id가 같은 경우, 작성자 email값 출력
+                    System.out.println(temp_author.getName() + " " + temp_author.getEmail() + " " + count);
+
+                    break;
+//                    게시글상세조회
+                case 5:
+                    System.out.println("post id를 입력해주세요");
+                    Long post_id = Long.parseLong(sc.nextLine());
+                    Post temp_post = null;
+                    for(int i=0; i<posts.size(); i++){
+                        if(posts.get(i).getId() == post_id){
+                            temp_post = posts.get(i);
+                            break;
+                        }
+                    }
+                    Author temp_post_author = null;
                     for(Author a : authors){
-                        if(p.getAuthor_id() == a.getId())
-                            System.out.println("작성자 email : " + a.getEmail());
-                        System.out.println();
+                        if(a.getId() == temp_post.getAuthor_id()){
+                            temp_post_author = a;
+                        }
                     }
-                }
+                    System.out.println(temp_post.getTitle() + " " + temp_post_author.getEmail());
+                    break;
             }
-            else System.out.println("잘못된 번호입니다.\n");
 
         }
     }
 }
-//    엔티티
-class Author {
-    //id, name, email, password
+
+//엔티티
+class Author{
     private Long id;
     private String name;
     private String email;
-    static Long static_id = 0L; //고유 id값 지정
-    Author(String name, String email) {
+    static Long static_id = 0L;
+    Author(String name, String email){
         static_id += 1;
         this.id = static_id;
         this.name = name;
         this.email = email;
-    }
-    @Override
-    public String toString() {
-        return "id :  " + id + "\nname : " + name + "\nemail : " + email + "\n";
     }
 
     public Long getId() {
@@ -129,24 +109,18 @@ class Author {
     }
 
 }
-class Post {
-    //id, title, contents, author_id
+class Post{
     private Long id;
     private String title;
     private String contents;
     private Long author_id;
-    static Long static_id = 0L;//고유 id값 지정
-
-    Post(String title, String contents, Long author_id) {
+    static Long static_id = 0L;
+    Post(String title, String contents, Long author_id){
         static_id += 1;
         this.id = static_id;
         this.title = title;
         this.contents = contents;
         this.author_id = author_id;
-    }
-    @Override
-    public String toString() {
-        return "id :  " + id + "\ntitle : " + title + "\ncontents : " + contents + "\nauthor_id : " + author_id + "\n";
     }
 
     public Long getId() {
